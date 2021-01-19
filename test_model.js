2d805170249cd2db669f4a1086412f2a1b5cc99c
jhk <jakub.kafarski@gmail.com>
2021-01-20 00:43:39 +0100

    eslinter

--- test_model.js
+++ test_model.js
@@ -1,24 +1,26 @@
 const fs = require('fs');
+const process = require('process');
 const { Network } = require('neataptic');
 const { FILE_NAME } = require('./constants');
 
 if (!fs.existsSync(FILE_NAME)) {
-    console.log('Error: Model ',FILE_NAME,' does not exist');
-    return false;
+  console.log('Error: Model ', FILE_NAME, ' does not exist');
+  process.exit();
 }
+
 const model = fs.readFileSync(FILE_NAME);
 const parsedModel = JSON.parse(model);
 const imported = Network.fromJSON(parsedModel);
 
 const extrapolationTests = [
-    { input: [13,13] },
-    { input: [88,2] },
-    { input: [123,100] },
-    { input: [-13,13] },
-    { input: [-13,0] },
-]
+  { input: [13, 13] },
+  { input: [1, 5] },
+  { input: [88, 2] },
+  { input: [123, 100] },
+  { input: [-13, 13] },
+  { input: [-13, 0] },
+];
 
-extrapolationTests.map((set) => {
-    console.log(set.input[0],' + ',set.input[1], ' = ' , Math.round(imported.activate(set.input)[0]))
-})
-
+extrapolationTests.forEach((set) => {
+  console.log(set.input[0], ' + ', set.input[1], ' = ', Math.round(imported.activate(set.input)[0]));
+});
--- train_model.js
+++ train_model.js
@@ -3,19 +3,25 @@ const { methods, architect } = require('neataptic');
 const { TRAINING_DATA_SET, FILE_NAME } = require('./constants');
 
 function trainNeuralNetwork() {
-        let network = new architect.LSTM(2,4,1);
-        network.evolve(TRAINING_DATA_SET, {
-                elitism: 10,
-                equal: true,
-                mutation: methods.mutation.ALL,
-                mutationRate: 0.8,
-                clear: true,
-                error: 0.03,
-                log: 100,
-                dropout: 0.3
-        });
-        const toSave = network.toJSON()
-        fs.writeFileSync(FILE_NAME, JSON.stringify(toSave));
+  const network = new architect.LSTM(2, 4, 1);
+  // let network = new architect.Perceptron(2,2,1);
+  // let network = new architect.GRU(2,4,1);
+  // let network = new architect.NARX(2,4,1,5,5);
+
+  const optionsLSTM = {
+    elitism: 10,
+    equal: true,
+    mutation: methods.mutation.ALL,
+    mutationRate: 0.8,
+    clear: true,
+    error: 0.03,
+    log: 100,
+    dropout: 0.3,
+  };
+
+  network.evolve(TRAINING_DATA_SET, optionsLSTM);
+  const toSave = network.toJSON();
+  fs.writeFileSync(FILE_NAME, JSON.stringify(toSave));
 }
 
-trainNeuralNetwork()
+trainNeuralNetwork();
